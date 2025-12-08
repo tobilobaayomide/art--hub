07595d67fdb2603a1320c6907184baa5cd3063cb
Your Name <your.email@example.com>
2025-12-08 22:25:57 +0100

    (refactor): update and clean up page components.

---
+++ client/src/pages/Drop.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Navbar from "../components/Navbar";
+
+const Drop: React.FC = () => {
+  return (
+    <>
+      <Navbar />
+    </>
+  );
+};
+
+export default Drop;
