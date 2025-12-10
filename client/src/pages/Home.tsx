2ab373582372a11d95ed5c96f9112f0f74a67767
Your Name <your.email@example.com>
2025-12-10 23:59:50 +0100

    (feat): add the navbar and footer feature

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,11 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
 
 const Home: React.FC = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+
   return (
-    <>
-      <Navbar />
-    </>
+    <div className="min-h-screen flex flex-col">
+      <Navbar menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
+      <main className="flex-1">
+        
+      </main>
+      <Footer menuOpen={menuOpen} />
+    </div>
   );
 };
 
