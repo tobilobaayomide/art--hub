27b35d4a49966a778a538c44cd603e196f9aeb24
Your Name <your.email@example.com>
2025-12-22 22:12:32 +0100

    (feat): integrate product page into marketplace

--- client/src/routes.tsx
+++ client/src/routes.tsx
@@ -2,10 +2,12 @@ import Home from './pages/Home';
 import Marketplace from './pages/Marketplace';
 import Auctions from './pages/Auctions';
 import Drop from './pages/Drop';
+import ProductPage from './marketplace/productpage'; 
 
 export const appRoutes = [
   { path: '/', element: <Home /> },
   { path: '/Marketplace', element: <Marketplace /> },
   { path: '/Auctions', element: <Auctions /> },
   { path: '/Drop', element: <Drop /> },
+  { path: '/Marketplace/product/:id', element: <ProductPage /> }, 
 ];
